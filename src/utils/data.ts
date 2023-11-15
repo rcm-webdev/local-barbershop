8e917fde7321fe0cbc4209e3578d480b6af033e4
rcm-webdev <rcm.webdev.pro@gmail.com>
2023-11-14 21:14:57 -0800

    update service icons to barbershop icons

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,40 +1,40 @@
 const services = [
-    {
-        title: "Social media marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-    },
-    {
-        title: "Amazon affiliate marketing",
-        description: "Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-    },
-    {
-        title: "Email marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-    },
-    {
-        title: "Social media marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-    },
-    {
-        title: "Amazon affiliate marketing",
-        description: "Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-    },
-    {
-        title: "Email marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-    },
+  {
+    title: "Social media marketing",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="stroke-black dark:stroke-white" viewBox="0 0 48 48"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path fill="#2F88FF" d="M29.7961 24H16.2039C16.2039 24 12.2176 31.7083 18.7524 34.8333C22.9346 37.3333 16.2039 44 16.2039 44H29.7962C29.7962 44 23.0654 36.7083 27.2476 34.8333C33.7824 31.7083 29.7961 24 29.7961 24Z"/><path d="M37 10L30 24H16L9 10C9 10 12 4 23 4C34 4 37 10 37 10Z"/><path d="M25 24L27 12"/><path d="M21 24L19 12"/></g></svg>`,
+  },
+  {
+    title: "Amazon affiliate marketing",
+    description:
+      "Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="stroke-black dark:stroke-white" viewBox="0 0 48 48"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M15 28V15.6522C15 13 18 10 24 10C30 10 33 13 33 15.6522V28"/><path fill="#2F88FF" d="M12 34V28H36V34H12Z"/><path d="M20 4H28"/><path d="M8 16V28H40V16"/><path d="M17 43L31 43"/><path d="M24 34V43"/><path d="M24 4V10"/></g></svg>`,
+  },
+  {
+    title: "Email marketing",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="stroke-black dark:stroke-white" viewBox="0 0 48 48"><g fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><rect width="38" height="38" x="5" y="5" fill="#2F88FF" stroke="currentColor" rx="3"/><path stroke="#fff" d="M19 19C21 22 21 26 19 29"/><path stroke="#fff" d="M36 14L21 24L36 34"/><circle cx="16" cy="16" r="4" stroke="#fff"/><circle cx="16" cy="32" r="4" stroke="#fff"/></g></svg>`,
+  },
+  {
+    title: "Social media marketing",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="stroke-black dark:stroke-white" viewBox="0 0 48 48"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M10 8L38 8V17L33 24V36C33 36 33 44 24 44C15 44 15 36 15 36L15 24L10 17V8Z"/><path d="M15 4V10"/><path d="M21 4V10"/><path d="M27 4V10"/><rect width="6" height="10" x="21" y="28" fill="#2F88FF" rx="3"/><path d="M10 17H38"/><path d="M33 4V10"/></g></svg>`,
+  },
+  {
+    title: "Amazon affiliate marketing",
+    description:
+      "Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="stroke-black dark:stroke-white"  viewBox="0 0 48 48"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><rect width="38" height="6" x="3.609" y="36.534" fill="#2F88FF" rx="2" transform="rotate(-10 3.61 36.534)"/><path d="M44 40L40 36"/><path fill="#2F88FF" d="M8 4L26.3848 22.3848L22.1421 26.6274C22.1421 26.6274 12.2426 16.7279 9.41419 13.8995C6.58577 11.071 6.58575 9.65682 6.58577 8.24262C6.58579 6.82843 8 4 8 4Z"/><path d="M8 4L26 22L35 31"/></g></svg>`,
+  },
+  {
+    title: "Email marketing",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="stroke-black dark:stroke-white" viewBox="0 0 48 48"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M18.1968 31.4243C21.321 34.5485 28.919 32.0158 35.1674 25.7674C41.4158 19.519 43.9484 11.9211 40.8242 8.79688"/><path d="M27.3887 6.67529L28.8029 8.08951"/><path d="M22.4395 11.625L23.8537 13.0392"/><path d="M18.1963 17.2822L19.6105 18.6964"/><path d="M16.7822 24.353L18.1964 25.7672"/><path d="M35.167 4.5542L36.5812 5.96841"/><path d="M33.7529 13.0396L36.5814 15.868"/><path d="M28.8037 17.9893L31.6321 20.8177"/><path d="M23.1465 22.2319L25.9749 25.0604"/><rect width="6" height="14" x="16.075" y="29.303" rx="3" transform="rotate(45 16.075 29.303)"/></g></svg>`,
+  },
 ];
 
 export { services };
